11ffa619fb4809e079ee81953e4511c5098f871e
trungnqth1809019 <trungnqth1809019@fpt.edu.vn>
2018-12-12 23:53:37 +0700

    up

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 document.forms['registerForm']['btnSubmit'].onclick = function () {
-    var phone = document.forms['registerForm']['phone'];
-    alert(phone.value);
-
-    // if(validateForm()){
-    //  doRegister();
-    // }
+    if (validateForm()) {
+        doRegister();
+    }
 };
 
+//ham kiem tra
 function validateForm() {
     var isValidate = true, isValidateName = true;
     var isValidatePhone = true, isValidateEmail = true;
-    
+
     //kiem tra name
     var txtName = document.forms['registerForm']['name'];
     var msgName = document.querySelector("[class*='msgName']");
@@ -19,31 +17,18 @@ function validateForm() {
         msgName.classList.add('msg-error');
         msgName.innerHTML = 'Không được để trống mục này!';
         isValidateName = false;
-    } else if(txtName.value >50){
+    } else if (txtName.value.length > 50) {
         msgName.classList.remove('msg-success');
         msgName.classList.add('msg-error');
         msgName.innerHTML = 'Tối đa 50 kí tự!';
         isValidateName = false;
-    }  else {
+    } else {
         msgName.classList.remove('msg-error');
         msgName.classList.add('msg-success');
         msgName.innerHTML = 'Hợp lệ.';
         isValidateName = true;
     }
-    //Kiem tra email
-    var txtEmail = document.forms['registerForm']['email'];
-    var msgEmail = document.querySelector("[class*='msgEmail']");
-    if (txtEmail.value == null || txtEmail.value.length == 0) {
-        msgEmail.classList.remove('msg-success');
-        msgEmail.classList.add('msg-error');
-        msgEmail.innerHTML = 'Không được để trống mục này!';
-        isValidateEmail = false;
-    } else {
-        msgEmail.classList.remove('msg-error');
-        msgEmail.classList.add('msg-success');
-        msgEmail.innerHTML = 'Hợp lệ.';
-        isValidateEmail = true;
-    }
+
     //Kiem tra phone
     var txtPhone = document.forms['registerForm']['phone'];
     var msgPhone = document.querySelector("[class*='msgPhone']");
@@ -58,26 +43,50 @@ function validateForm() {
         msgPhone.innerHTML = 'Hợp lệ.';
         isValidatePhone = true;
     }
-    
+
+    //Kiem tra email
+    var txtEmail = document.forms['registerForm']['email'];
+    var msgEmail = document.querySelector("[class*='msgEmail']");
+    if (txtEmail.value == null || txtEmail.value.length == 0) {
+        msgEmail.classList.remove('msg-success');
+        msgEmail.classList.add('msg-error');
+        msgEmail.innerHTML = 'Không được để trống mục này!';
+        isValidateEmail = false;
+    } else {
+        msgEmail.classList.remove('msg-error');
+        msgEmail.classList.add('msg-success');
+        msgEmail.innerHTML = 'Hợp lệ.';
+        isValidateEmail = true;
+    }
+
     //kiem tra isvalidate
-    isValidate = isValidateName && isValidateLastName && isValidatePassword && isValidateConfirmPassword && isValidateAddress && isValidatePhone && isValidateAvatar && isValidateEmail;
+    isValidate = isValidateName && isValidatePhone && isValidateEmail;
     return isValidate;
 }
 
+//ham hien thi thong tin moi nhap
 function doRegister() {
-    var txtName = document.forms['registerForm']['name'].value;
-    var txtEmail = document.forms['registerForm']['email'].value;
-    var txtPhone = document.forms['registerForm']['phone'].value;
-    var txtGender = document.forms['registerForm']['gender'].value;
-    var txtLike = document.forms['registerForm']['like'];
-    var txtIntroduction = document.forms['registerForm']['introduction'].value;
+    alert('Đăng ký thông tin thành công');
+    var name = document.forms['registerForm']['name'].value;
+    var email = document.forms['registerForm']['email'].value;
+    var phone = document.forms['registerForm']['phone'].value;
+    var gender = document.forms['registerForm']['gender'].value;
+    var txtHobby = document.forms['registerForm']['hobby'];
+    var hobby = '';
+    for(var i = 0; i < txtHobby.length; i++){
+        if (txtHobby[i].checked){
+            hobby = hobby + txtHobby[i].value + ', ';
+        }
+    }
+    var introduction = document.forms['registerForm']['introduction'].value;
     var mainContent = document.getElementById('main-content');
     var htmlContent = '';
-    htmlContent += '<p>Họ và tên: ' + txtName + '</p>';
-    htmlContent += '<p>Email: '+ txtEmail + '</p>';
-    htmlContent += '<p>Phone: '+ txtPhone + '</p>';
-    htmlContent += '<p>Giới tính: '+ txtGender + '</p>';
-    htmlContent += '<p>Sở thích: '+ txtLike + '</p>';
-    htmlContent += '<p>Giới thiệu về bản thân: ' + txtIntroduction + '</p>';
+    htmlContent += '<h3> Thông tin vừa nhập</h3>';
+    htmlContent += '<p>Họ và tên: ' + name + '</p>';
+    htmlContent += '<p>Email: ' + email + '</p>';
+    htmlContent += '<p>Phone: ' + phone + '</p>';
+    htmlContent += '<p>Giới tính: ' + gender + '</p>';
+    htmlContent += '<p>Sở thích: ' + hobby + '</p>';
+    htmlContent += '<p>Giới thiệu về bản thân: ' + introduction + '</p>';
     mainContent.innerHTML = htmlContent;
 }
